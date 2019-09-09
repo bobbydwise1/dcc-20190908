79ca4352d6c898129e79af344702202a4b684299
Robert Lee <bobbydwise1@hotmail.com>
2019-09-08 20:14:33 -0700

    Create the final solution.

--- scripts.js
+++ scripts.js
@@ -11,17 +11,44 @@ You can assume all the integers in the array are unique.
 */
 
 // This is the javascript "default" solution
-const finder = (yourArray, term) => {
-  let found = yourArray.findIndex(
-    function(element) {
-      return element == term
-    })
-  return found;
+// const finder = (yourArray, term) => {
+//   let found = yourArray.findIndex(
+//     function(element) {
+//       return element == term
+//     })
+//   return found;
+// }
+
+// We note that the array is really two arrays that are sorted.  If the element you are searching for is smaller than the 1st element of the 1st array, it's faster to search from the end of the 2nd array.
+const finder = (yourArray,term) => {
+  //If your search term is greater than the 1st element, do a normal left-to-right search on the array.
+  if (term >= yourArray[0]) {
+    let skipped = 0;
+    for ( i = 0; i < yourArray.length; i++ ) {
+      if ( yourArray[i] != term ) {
+        skipped++
+      } else {
+        return skipped
+      }
+    }
+  } else { //Otherwise, if your search term is smaller than the 1st element, do a right-to-left search on the array.
+    let skipped = yourArray.length-1
+    for ( i = yourArray.length-1; i >= 0; i-- ) {
+      if ( yourArray[i] != term ) {
+        skipped--
+      } else {
+        return skipped
+      }
+    }
+  }
+  return null
 }
 
-const test0 = [13, 18, 25, 2, 8, 10]
-const test1 = 8
-const result0 = 4
+//In theory, this should always be faster than linear time, assumming a good mid-point distribution of the rotated array.
+
+// const test0 = [13, 18, 25, 2, 8, 10]
+// const test1 = 8
+// const result0 = 4
 
 console.log(finder(test0,test1))
 
